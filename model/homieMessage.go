9f7c0f3d34ef5e4f943caabf4e0fe31c891f67d8
Julien BONACHERA <julien.bonachera@smile.fr>
2017-01-14 22:50:36 +0100

    handles deviceId extraction in NewHomieMessage()

--- model/homieMessage.go
+++ model/homieMessage.go
@@ -14,13 +14,16 @@ type HomieMessage struct {
 	Topic     string
 	Payload   string
 	BaseTopic string
+	Id        string
 }
 
 func NewHomieMessage(m HomieExtractableMessage, baseTopic string) (HomieMessage, error) {
-	message := HomieMessage{m.Topic(), string(m.Payload()), baseTopic}
-	if !strings.HasPrefix(message.Topic, message.BaseTopic) {
+	message := HomieMessage{m.Topic(), string(m.Payload()), baseTopic, ""}
+	strippedPrefix := strings.TrimPrefix(message.Topic, message.BaseTopic)
+	if strippedPrefix == message.Topic {
 		return HomieMessage{}, errors.New("Topic does not start with BaseTopic")
 	}
+	message.Id = strings.Split(strippedPrefix, "/")[0]
 	return message, nil
 }
 
