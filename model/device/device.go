36feca8f5fdc0708ddcec870db97a52f76865ce0
Julien BONACHERA <julien.bonachera@smile.fr>
2017-01-14 23:58:36 +0100

    add Set() method

--- model/device/device.go
+++ model/device/device.go
@@ -1,5 +1,9 @@
 package device
 
+import (
+	"strconv"
+)
+
 type DeviceStats struct {
 	Uptime   int
 	Signal   int
@@ -25,4 +29,30 @@ type Device struct {
 
 func New(id string) Device {
 	return Device{id, false, "", "", "", DeviceStats{0, 0, 0}, DeviceFirmware{"", "", ""}, ""}
+}
+func (d *Device) Set(prop string, value string) {
+	switch prop {
+	case "$online":
+		d.Online, _ = strconv.ParseBool(value)
+	case "$name":
+		d.Name = value
+	case "$localip":
+		d.Localip = value
+	case "$mac":
+		d.Mac = value
+	case "$stats/uptime":
+		d.Stats.Uptime, _ = strconv.Atoi(value)
+	case "$stats/signal":
+		d.Stats.Signal, _ = strconv.Atoi(value)
+	case "$stats/interval":
+		d.Stats.Interval, _ = strconv.Atoi(value)
+	case "$fw/name":
+		d.Fw.Name = value
+	case "$fw/version":
+		d.Fw.Version = value
+	case "$fw/checksum":
+		d.Fw.Checksum = value
+	case "$implementation":
+		d.Implementation = value
+	}
 }
