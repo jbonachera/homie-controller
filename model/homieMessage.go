ac903b34db32fcb180286c55a0166bb68cd27b63
Julien BONACHERA <julien.bonachera@smile.fr>
2017-01-14 22:37:33 +0100

    return error when creating HomieMessage

--- model/homieMessage.go
+++ model/homieMessage.go
@@ -13,13 +13,16 @@ type HomieMessage struct {
 	BaseTopic string
 }
 
-func NewHomieMessage(m HomieExtractableMessage, baseTopic string) HomieMessage {
-	return HomieMessage{m.Topic(), string(m.Payload()), baseTopic}
+func NewHomieMessage(m HomieExtractableMessage, baseTopic string) (HomieMessage, error) {
+	return HomieMessage{m.Topic(), string(m.Payload()), baseTopic}, nil
 }
 
 func (m HomieMessage) DeviceId() string {
 	// Remove the baseTopic from the topic by reading the string
 	// juste after it.
-	strippedBase := m.Topic[len(m.BaseTopic):]
-	return strings.Split(strippedBase, "/")[0]
+	if strings.Contains(m.Topic, m.BaseTopic) {
+		strippedBase := m.Topic[len(m.BaseTopic):]
+		return strings.Split(strippedBase, "/")[0]
+	}
+	return ""
 }
