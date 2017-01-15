e3830088addca28342a9690fcbb941352c9176af
Julien BONACHERA <julien.bonachera@smile.fr>
2017-01-15 22:27:16 +0100

    add factory

---
+++ model/device/nodetype/temperature/temperatureFactory_test.go
@@ -0,0 +1,13 @@
+package temperature
+
+import (
+	"github.com/jbonachera/homie-controller/model/device/nodetype"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	temperature := nodetype.New("temperature", "devices/")
+	if temperature.GetType() != "temperature" {
+		t.Error("could not get a tempetature node type")
+	}
+}
