4effd27cd350ca1d8fdeaaac05483e1b3b18f6e5
Julien BONACHERA <julien@bonachera.fr>
2017-01-16 18:23:05 +0100

    registry is now a package-global var

--- model/device/registry.go
+++ model/device/registry.go
@@ -14,59 +14,62 @@ type Registry struct {
 	baseTopic    string
 }
 
-func NewRegistry(baseTopic string) Registry {
-	return Registry{sync.Mutex{}, []Device{}, map[string]int{}, baseTopic}
+var registry Registry
+
+
+func NewRegistry(baseTopic string) {
+	registry = Registry{sync.Mutex{}, []Device{}, map[string]int{}, baseTopic}
 }
 
 func addToIndex(index map[string]int, key string, offset int) {
 	index[key] = offset
 }
 
-func (d *Registry) Append(device Device) {
-	d.Lock()
-	defer d.Unlock()
-	d.devices = append(d.devices, device)
-	addToIndex(d.devicesIndex, device.Id, len(d.devices)-1)
+func Append(device Device) {
+	registry.Lock()
+	defer registry.Unlock()
+	registry.devices = append(registry.devices, device)
+	addToIndex(registry.devicesIndex, device.Id, len(registry.devices)-1)
 }
 
-func (d Registry) Get(id string) Device {
-	d.Lock()
-	defer d.Unlock()
-	offset, ok := d.devicesIndex[id]
+func Get(id string) Device {
+	registry.Lock()
+	defer registry.Unlock()
+	offset, ok := registry.devicesIndex[id]
 	if ok {
-		return d.devices[offset]
+		return registry.devices[offset]
 	}
 	return Device{}
 
 }
 
-func (d *Registry) Set(id string, path string, value string) {
-	d.Lock()
-	defer d.Unlock()
-	offset, ok := d.devicesIndex[id]
+func Set(id string, path string, value string) {
+	registry.Lock()
+	defer registry.Unlock()
+	offset, ok := registry.devicesIndex[id]
 	if ok {
-		d.devices[offset].Set(path, value)
+		registry.devices[offset].Set(path, value)
 	}
 }
-func (d *Registry) DeviceOnlineCallback(client MQTT.Client, mqttMessage MQTT.Message) {
-	message, err := homieMessage.New(mqttMessage, d.baseTopic)
+func DeviceOnlineCallback(client MQTT.Client, mqttMessage MQTT.Message) {
+	message, err := homieMessage.New(mqttMessage, registry.baseTopic)
 	if err != nil {
 		log.Warn("received an invalid message")
 		return
 	}
 	if message.Payload == "true" {
 		log.Debug("discovered a new device: " + message.Id)
-		device := New(message.Id, d.baseTopic)
-		d.Append(device)
+		device := New(message.Id, registry.baseTopic)
+		Append(device)
 		for _, prop := range homieMessage.Properties {
-			client.Subscribe(d.baseTopic+message.Id+"/"+prop, 1, device.MQTTNodeHandler)
+			client.Subscribe(registry.baseTopic+message.Id+"/"+prop, 1, device.MQTTNodeHandler)
 		}
-		client.Subscribe(d.baseTopic+message.Id+"/+/$type", 1, device.MQTTNodeHandler)
+		client.Subscribe(registry.baseTopic+message.Id+"/+/$type", 1, device.MQTTNodeHandler)
 	} else {
 		log.Debug("a device has disconnected: " + message.Id)
 		for _, prop := range homieMessage.Properties {
-			client.Unsubscribe(d.baseTopic + message.Id + "/" + prop)
+			client.Unsubscribe(registry.baseTopic + message.Id + "/" + prop)
 		}
-		client.Unsubscribe(d.baseTopic + message.Id + "/+/$type")
+		client.Unsubscribe(registry.baseTopic + message.Id + "/+/$type")
 	}
 }
--- model/device/registry_test.go
+++ model/device/registry_test.go
@@ -9,12 +9,12 @@ import (
 var baseTopic string = "devices/"
 
 func TestAppend(t *testing.T) {
-	registry := NewRegistry(baseTopic)
-	registry.Append(New("u1", baseTopic))
+	NewRegistry(baseTopic)
+	Append(New("u1", baseTopic))
 	if registry.devices[0].Id != "u1" {
 		t.Error("didn't get the device we just inserted")
 	}
-	registry.Append(New("u2", baseTopic))
+	Append(New("u2", baseTopic))
 	if registry.devices[0].Id != "u1" {
 		t.Error("existing device disapeared after insertion")
 	}
@@ -23,22 +23,22 @@ func TestAppend(t *testing.T) {
 	}
 }
 
-func appendList(count int, r *Registry, wg *sync.WaitGroup) {
+func appendList(count int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	i := 0
 	for i < count {
-		r.Append(New("u"+strconv.Itoa(i), baseTopic))
+		Append(New("u"+strconv.Itoa(i), baseTopic))
 		i += 1
 	}
 }
 func TestParralellAppend(t *testing.T) {
 	var wg sync.WaitGroup
 	count := 100
-	registry := NewRegistry(baseTopic)
+	NewRegistry(baseTopic)
 	wg.Add(count)
 	i := 0
 	for i < count {
-		go appendList(200, &registry, &wg)
+		go appendList(200, &wg)
 		i += 1
 	}
 	wg.Wait()
@@ -48,53 +48,53 @@ func TestParralellAppend(t *testing.T) {
 }
 
 func populate(count int) *Registry {
-	registry := NewRegistry(baseTopic)
+	NewRegistry(baseTopic)
 	i := 0
 	for i < count {
-		registry.Append(New("u"+strconv.Itoa(i), "devices/"))
+		Append(New("u"+strconv.Itoa(i), "devices/"))
 		i += 1
 	}
 	return &registry
 }
 func TestGet(t *testing.T) {
-	registry := populate(30)
-	device := registry.Get("u17")
+	populate(30)
+	device := Get("u17")
 	if device.Id != "u17" {
 		t.Error("could not Get() a created device: got ", device.Id, " , wanted u17")
 	}
 
 }
 func TestRegistrySet(t *testing.T) {
-	registry := populate(30)
-	registry.Set("u17", "$online", "true")
+	populate(30)
+	Set("u17", "$online", "true")
 	if registry.devices[17].Online != true {
 		t.Error("could not Set() a property: wanted true, got", registry.devices[17].Online)
 	}
 }
 
-func setProp(count int, prop string, id string, value string, r *Registry, wg *sync.WaitGroup) {
+func setProp(count int, prop string, id string, value string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	i := 0
 	for i < count {
-		r.Set(id, prop, value)
+		Set(id, prop, value)
 		i += 1
 	}
 }
 func TestParrallelSet(t *testing.T) {
-	r := populate(30)
+	populate(30)
 	var wg sync.WaitGroup
 	i := 0
 	wg.Add(30 * 3)
 	for i < 30 {
-		go setProp(20, "$online", "u17", "false", r, &wg)
-		go setProp(20, "$online", "u17", "true", r, &wg)
-		go setProp(20, "$stats/signal", "u17", "87", r, &wg)
+		go setProp(20, "$online", "u17", "false", &wg)
+		go setProp(20, "$online", "u17", "true", &wg)
+		go setProp(20, "$stats/signal", "u17", "87", &wg)
 		i += 1
 	}
 	wg.Wait()
-	device := r.Get("u17")
-	if device.Stats.Signal != 87 {
-		t.Error("could not set property: wanted 87, got", device.Stats.Signal)
+	myDevice := Get("u17")
+	if myDevice.Stats.Signal != 87 {
+		t.Error("could not set property: wanted 87, got", myDevice.Stats.Signal)
 	}
 }
 
@@ -112,9 +112,9 @@ func BenchmarkSet10000(b *testing.B) {
 	benchmarkSet(10000, b)
 }
 func benchmarkSet(j int, b *testing.B) {
-	r := populate(j)
+	populate(j)
 	for i := 0; i < b.N; i++ {
-		r.Set("u988", "$online", "true")
+		Set("u988", "$online", "true")
 	}
 }
 
@@ -133,8 +133,8 @@ func BenchmarkGet10000(b *testing.B) {
 }
 
 func benchmarkGet(j int, b *testing.B) {
-	r := populate(j)
+	populate(j)
 	for i := 0; i < b.N; i++ {
-		r.Get("u988")
+		Get("u988")
 	}
 }
