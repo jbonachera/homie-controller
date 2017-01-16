2a27bf058c0168aee20848f8b4f79862b66d9ca9
Julien BONACHERA <julien@bonachera.fr>
2017-01-16 17:33:25 +0100

    split node/type and node/registry

--- model/node/registry.go
+++ model/node/registry.go
@@ -2,23 +2,14 @@ package node
 
 import (
 	"errors"
-	MQTT "github.com/eclipse/paho.mqtt.golang"
-	"github.com/jbonachera/homie-controller/model/metric"
 )
 
 var Factories = make(map[string]Factory)
 
-type Type interface {
-	GetName() string
-	GetType() string
-	GetProperties() []string
-	GetPoint() metric.Metric
-	MQTTHandler(mqttClient MQTT.Client, message MQTT.Message)
-}
-
 type Factory interface {
 	New(name string, baseTopic string) Type
 }
+
 
 func RegisterNodeTypeFactory(name string, nodeType Factory) {
 	_, exist := Factories[name]
---
+++ model/node/type.go
@@ -0,0 +1,16 @@
+package node
+
+import (
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"github.com/jbonachera/homie-controller/model/metric"
+)
+
+
+type Type interface {
+	GetName() string
+	GetType() string
+	GetProperties() []string
+	GetPoint() metric.Metric
+	MQTTHandler(mqttClient MQTT.Client, message MQTT.Message)
+}
+
