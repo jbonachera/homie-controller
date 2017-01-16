8ed119a8f0570f4ad10925047f748d5419c76d10
Julien BONACHERA <julien.bonachera@smile.fr>
2017-01-16 01:47:32 +0100

    add mqtt client mock implem
    thanks https://github.com/eclipse/paho.mqtt.golang/issues/73

---
+++ mocks/mqtt/client.go
@@ -0,0 +1,69 @@
+package mqtt
+
+import (
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+type MessageMock struct {
+	topic   string
+	payload string
+}
+
+func (m MessageMock) Topic() string {
+	return m.topic
+}
+func (m MessageMock) Payload() []byte {
+	return []byte(m.payload)
+}
+func (m MessageMock) Duplicate() bool {
+	return false
+}
+func (m MessageMock) Qos() byte {
+	return byte(0)
+}
+func (m MessageMock) Retained() bool {
+	return true
+}
+func (m MessageMock) MessageID() uint16 {
+	return uint16(10)
+}
+
+type MockClient struct {
+	isConnected bool
+	Topic       string
+}
+
+func (mc *MockClient) IsConnected() bool {
+	return mc.isConnected
+}
+
+func (mc *MockClient) Connect() MQTT.Token {
+	return nil
+}
+
+func (mc *MockClient) Disconnect(quiesce uint) {
+	return
+}
+
+func (mc *MockClient) Publish(topic string, qos byte, retained bool, payload interface{}) MQTT.Token {
+	return nil
+}
+func (mc *MockClient) Subscribe(topic string, qos byte, callback MQTT.MessageHandler) MQTT.Token {
+	mc.Topic = topic
+	return nil
+}
+
+func (mc *MockClient) SubscribeMultiple(filters map[string]byte, callback MQTT.MessageHandler) MQTT.Token {
+	return nil
+}
+
+func (mc *MockClient) Unsubscribe(topics ...string) MQTT.Token {
+	return nil
+}
+
+func NewMessage(topic string, payload string) MessageMock {
+	return MessageMock{topic, payload}
+}
+func NewMockClient(connected bool, topic string) *MockClient {
+	return &MockClient{connected, topic}
+}
