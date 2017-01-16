21fa90bf1149ab57a3f24c39b582cc003c693962
Julien BONACHERA <julien@bonachera.fr>
2017-01-16 17:57:34 +0100

    add start() methods to tests, and test getenv logic

--- config/config_test.go
+++ config/config_test.go
@@ -1,11 +1,33 @@
 package config
 
-import "testing"
+import (
+	"testing"
+	"os"
+)
 
 func TestGet(t *testing.T) {
+	start()
 	storage["key"] = "value"
 	testKey := Get("key")
 	if testKey != "value"{
 		t.Error("did not get a valid value: got ", testKey, ", wanted key")
+	}
+}
+func TestGetEnv(t *testing.T) {
+	start()
+	os.Setenv("KEY", "value2")
+	testKey := Get("key")
+	if testKey != "value2"{
+		t.Error("did not get a valid value: got ", testKey, ", wanted value2")
+	}
+}
+
+func TestGetEnvOverride(t *testing.T) {
+	start()
+	storage["key"] = "value"
+	os.Setenv("key", "value2")
+	testKey := Get("key")
+	if testKey != "value"{
+		t.Error("did not get a valid value: got ", testKey, ", wanted value")
 	}
 }
