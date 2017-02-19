174fedb8a74899a94059bf33a16a90bc0b869a86
Julien BONACHERA <julien@bonachera.fr>
2017-02-19 18:06:56 +0100

    rollback mqtt async operations

--- messaging/client.go
+++ messaging/client.go
@@ -3,58 +3,54 @@ package messaging
 import (
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"github.com/jbonachera/homie-controller/log"
-	"time"
 	"github.com/jbonachera/homie-controller/model/homieMessage"
+	"time"
 )
 
-var c MQTT.Client
-var baseTopic string
 type CallbackHandler func(message homieMessage.HomieMessage)
-var connected bool = false
 
-func Start(broker string, client_id string, mqttBaseTopic string) {
-	opts := MQTT.NewClientOptions().AddBroker("tcp://" + broker + ":1883")
+type messagingBroker struct {
+	c         MQTT.Client
+	baseTopic string
+	connected bool
+}
+
+var broker messagingBroker
+
+func Start(brokerHost string, client_id string, mqttBaseTopic string) {
+	opts := MQTT.NewClientOptions().AddBroker("tcp://" + brokerHost + ":1883")
 	opts.SetClientID(client_id)
-	c = MQTT.NewClient(opts)
-	baseTopic = mqttBaseTopic
-	connected = false
-	for !connected {
-		if token := c.Connect(); token.Wait() && token.Error() != nil {
+	broker = messagingBroker{MQTT.NewClient(opts), mqttBaseTopic, false}
+	for !broker.connected {
+		if token := broker.c.Connect(); token.Wait() && token.Error() != nil {
 			log.Error("could not connect to MQTT.")
 			time.Sleep(5 * time.Second)
 		} else {
 			log.Info("connected to MQTT broker")
-			connected = true
+			broker.connected = true
 		}
 	}
 }
-func asyncDelSubscription(topic string){
-	for !connected {
+func DelSubscription(topic string) {
+	for !broker.connected {
 		log.Info("waiting for MQTT connection to start..")
 		time.Sleep(2 * time.Second)
 	}
-	log.Debug("Unsubscribing to "+topic)
-	c.Unsubscribe(topic)
+	log.Debug("Unsubscribing to " + topic)
+	broker.c.Unsubscribe(topic)
 }
-func DelSubscription(topic string){
-	go asyncDelSubscription(topic)
-}
-
 func AddSubscription(topic string, qos byte, callback MQTT.MessageHandler) {
-	go asyncAddSubscription(topic, qos, callback)
-}
-func asyncAddSubscription(topic string, qos byte, callback MQTT.MessageHandler){
-	for !connected {
+	for !broker.connected {
 		log.Info("waiting for MQTT connection to start..")
 		time.Sleep(2 * time.Second)
 	}
-	log.Debug("Subscribing to "+topic)
-	c.Subscribe(topic, qos, callback)
+	log.Debug("Subscribing to " + topic)
+	broker.c.Subscribe(topic, qos, callback)
 }
 
-func AddHandler(topic string, callback CallbackHandler){
-	AddSubscription(topic, 0, func(mqttClient MQTT.Client, mqttMessage MQTT.Message){
-		message, err := homieMessage.New(mqttMessage, baseTopic)
+func AddHandler(topic string, callback CallbackHandler) {
+	AddSubscription(topic, 0, func(mqttClient MQTT.Client, mqttMessage MQTT.Message) {
+		message, err := homieMessage.New(mqttMessage, broker.baseTopic)
 		if err != nil {
 			log.Error("malformed message")
 		} else {
--- messaging/client_test.go
+++ messaging/client_test.go
@@ -1,19 +1,20 @@
 package messaging
 
 import (
-	"testing"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"github.com/jbonachera/homie-controller/mocks/mqtt"
 	"github.com/jbonachera/homie-controller/model/homieMessage"
+	"testing"
+	"time"
 )
 
 func callback(_ MQTT.Client, _ MQTT.Message) {}
 
-
 func TestAddSubscription(t *testing.T) {
 	mock := mqtt.NewMockClient(true, "old/topic")
-	c = mock
+	broker = messagingBroker{mock, "devices/", true}
 	AddSubscription("devices/bah/+", 0, callback)
+	time.Sleep(300 * time.Millisecond)
 	if mock.Topic != "devices/bah/+" {
 		t.Error("could not add subscription")
 	}
@@ -21,7 +22,7 @@ func TestAddSubscription(t *testing.T) {
 
 func TestDelSubscription(t *testing.T) {
 	mock := mqtt.NewMockClient(true, "old/topic")
-	c = mock
+	broker = messagingBroker{mock, "devices/", true}
 	AddSubscription("devices/bah/+", 0, callback)
 	DelSubscription("devices/bah/+")
 	if mock.Topic == "devices/bah/+" {
@@ -30,8 +31,8 @@ func TestDelSubscription(t *testing.T) {
 }
 func TestAddHandler(t *testing.T) {
 	mock := mqtt.NewMockClient(true, "old/topic")
-	c = mock
-	AddHandler("devices/bah/+", func(message homieMessage.HomieMessage){})
+	broker = messagingBroker{mock, "devices/", true}
+	AddHandler("devices/bah/+", func(message homieMessage.HomieMessage) {})
 	if mock.Topic != "devices/bah/+" {
 		t.Error("could not add handler")
 	}
