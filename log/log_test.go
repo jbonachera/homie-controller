b364f67204eecca414778be66f72d015aa544374
Julien BONACHERA <julien@bonachera.fr>
2017-02-19 14:38:24 +0100

    add more tests for log

--- log/log_test.go
+++ log/log_test.go
@@ -5,3 +5,20 @@ import "testing"
 func TestInfo(t *testing.T) {
 	Info("test")
 }
+
+func TestSetLogLevel(t *testing.T) {
+	SetLogLevel("DEBUG")
+	if defaultLogger.getLogLevel() != DEBUG {
+		t.Error("could not set log level to DEBUG")
+	}
+	if defaultLogger.getLogLevel() < INFO {
+		t.Error("INFO loglevel is lower than DEBUG")
+	}
+	SetLogLevel("ERROR")
+	if defaultLogger.getLogLevel() != ERROR {
+		t.Error("could not set log level to ERROR")
+	}
+	if defaultLogger.getLogLevel() > INFO {
+		t.Error("INFO loglevel is bigger than ERROR")
+	}
+}
