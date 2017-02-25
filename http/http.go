2761ba547f08b14ba500e4f476152abcfa8c3406
Julien BONACHERA <julien@bonachera.fr>
2017-02-25 15:40:58 +0100

    expose implementation actions in REST API

--- http/http.go
+++ http/http.go
@@ -10,9 +10,15 @@ import (
 
 var router = vestigo.NewRouter()
 
+type statusMessage struct {
+	Error bool `json:"error"`
+	Msg string `json:"msg"`
+}
+
 func Start() {
 	router.Get("/devices/", ListDevicesHandler)
 	router.Get("/device/:name", GetDeviceHandler)
+	router.Post("/device/:name/implementation/:action", PostImplementationActionHandler)
 	log.Info("starting HTTP API")
 	log.Error(http.ListenAndServe(":8989", router).Error())
 }
@@ -29,4 +35,15 @@ func GetDeviceHandler(w http.ResponseWriter, r *http.Request) {
 func ListDevicesHandler(w http.ResponseWriter, _ *http.Request) {
 	json.NewEncoder(w).Encode(device.List())
 	w.WriteHeader(200)
+}
+func PostImplementationActionHandler(w http.ResponseWriter, r *http.Request) {
+	name := vestigo.Param(r, "name")
+	myDevice, err := device.Get(name)
+	if err != nil {
+		w.WriteHeader(404)
+	} else {
+		action := vestigo.Param(r, "action")
+		myDevice.Implementation.Do(action)
+		json.NewEncoder(w).Encode(statusMessage{Error: false, Msg: "command sent"})
+	}
 }
