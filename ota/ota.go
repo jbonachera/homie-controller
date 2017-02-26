8ffe517c3ca83bca942260b4fbfea7714ad0dab4
Julien BONACHERA <julien@bonachera.fr>
2017-02-26 17:27:12 +0100

    add ota

---
+++ ota/ota.go
@@ -0,0 +1,42 @@
+package ota
+
+import (
+	"github.com/mcuadros/go-version"
+)
+
+var firmwares map[string]FirmwareProvider
+var factories map[string]FirmwareFactory
+
+type Firmware interface {
+	Name() string
+	Version() string
+	Checksum() string
+	Payload() []byte
+}
+
+type FirmwareProvider interface {
+	Id() string
+	GetLatest() Firmware
+}
+
+type FirmwareFactory interface {
+	New(name string) FirmwareProvider
+	Id() string
+}
+
+func init() {
+	factories = map[string]FirmwareFactory{}
+	firmwares = map[string]FirmwareProvider{}
+}
+
+func RegisterFactory(name string, provider FirmwareFactory) {
+	factories[name] = provider
+}
+
+func AddFirmware(name string, provider string) {
+	firmwares[name] = factories[provider].New(name)
+}
+
+func IsUpToDate(firmware string, current_version string) bool {
+	return version.Compare(current_version, firmwares[firmware].GetLatest().Version(), ">=")
+}
